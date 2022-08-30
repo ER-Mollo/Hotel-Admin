a7cc4180636534b872c74c5c55549920ba23a640
unknown <kevinmahlaule@gmail.com>
2022-08-30 11:50:23 +0200

    added addroom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 // import './App.css';
 import Dashboard from './components/dashboard';
+import AddRoom from './components/addroom';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard/>
+      <AddRoom/>
     </div>
   );
 }
