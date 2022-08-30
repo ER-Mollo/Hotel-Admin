651e44cc038a265bfa11ec5d7581dcb59bc65321
mahlaule <kevinmahlaule@gmail.com>
2022-08-30 12:40:22 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,4 @@
-import logo from './logo.svg';
-// import './App.css';
-import Dashboard from './components/dashboard';
-<<<<<<< HEAD
 import AddRoom from './components/addroom';
-
-function App() {
-  return (
-    <div className="App">
-      <AddRoom/>
-    </div>
-=======
 import Login from './components/login';
 import Dashboard from './components/dashboard';
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
@@ -19,10 +8,11 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Login/>}/>
-        <Route exact path="/" element={<Dashboard/>}/>
+        <Route exact path="/dashboard" element={<Dashboard/>}/>
+        <Route exact path="/addroom" element={<AddRoom/>}/>
       </Routes>
     </BrowserRouter>
->>>>>>> 86d1142056d281957803f68c31f95f8fcf352262
+
   );
 }
 
